58d2821a64c329f42391ba685b552ef917f7f41e
Enrique Juan de Dios <juandedioz@gmail.com>
2017-09-03 21:46:49 -0500

    Added Vue to Front-End

--- project/resources/assets/js/app.js
+++ project/resources/assets/js/app.js
@@ -1,22 +1,23 @@
+import Vue from 'vue';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-require('./bootstrap');
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios,axios);
 
-window.Vue = require('vue');
+import App from './App.vue';
+import Example from './components/Example.vue';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const routes = [
+  {
+      name: 'Example',
+      path: '/',
+      component: Example
+  }
+];
 
-Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+const router = new VueRouter({mode:'history',routes:routes});
+new Vue(Vue.util.extend({ router },App)).$mount('#app');
